d4e22722077f5a543c87455000674fd2c3ef83d0
James Murphy <james.f.murphy.one@gmail.com>
2017-05-21 18:35:58 -0400

    acquire names more efficiently

--- names.py
+++ names.py
@@ -42,30 +42,44 @@ def parse(msg):
 	except IndexError as e:
 		return []
 	
+	# obtain the race of origin and the types of names (masc, fem, sur)
 	race, flags = translation[command[0]], command[1:]
 	try:
 		flags = [translation[flag.lower()] for flag in flags]
 	except KeyError as e:
-		return ['bad name: `%s`' % e]
-	return [name_query(race, flag) for flag in flags]
+		return ['no names for race `%s`' % e]
+	
+	unique = list(set(flags))
+	
+	names_by_type = { }
+	for flag in unique:
+		names_by_type[flag] = name_query(race, flag, flags.count(flag))
+	
+	output = []
+	for flag in flags:
+		output.append(names_by_type[flag].pop())
+	return output
 	
 def message(names):
 	return ' '.join(names)
 
-def name_query(race, flag):
+def name_query(race, flag, count):
 	con = connect()
 	cur = con.cursor()
 	
 	query = cur.mogrify(
 		'select name from names where race = %s and nametype = %s and '
-		+ 'random() < 0.01 limit 1',
-		(race, flag)
+		+ 'random() < 0.01 limit %s',
+		(race, flag, count)
 	)
-	print(query)
 	cur.execute(query)
 	results = cur.fetchall()
-	print(results)
-	return results[0][0]
+	try:
+		out = list(results[0])
+	except IndexError as e:
+		out = []
+	return out
+	
 	
 
 
