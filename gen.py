74dc1e4c87b172e539c2259ddc04585bd3bb1257
James Murphy <james.f.murphy.one@gmail.com>
2017-08-22 21:26:37 -0400

    working on character generator

---
+++ gen.py
@@ -0,0 +1,45 @@
+import random
+import char
+import names
+import dice
+
+races = [
+	race for race in names.translation.keys() if len(
+		names.translation[race]
+	) > 1
+]
+
+genders = [
+	gender for gender in names.translation.keys() if len(
+		names.translation[gender]
+	) < 2 and gender[0] != 's'
+]
+
+def parse(msg):
+	if "!gen" not in msg:
+		return []
+	tokens = msg.casefold().split('!gen')
+	try:
+		tokens = [token.strip() for token in tokens[1].split()]
+	except IndexError as e:
+		argc = 0
+	else:
+		argc = len(tokens)
+	
+	race = names.translation[random.choice(races)]
+	gender = names.translation[random.choice(genders)]
+	
+	if argc >= 1:
+		race = names.translation[token[0]] if token[0] in races else race
+	if argc >= 2:
+		gender = names.translation[token[1]] if token[1] in genders else gender
+	
+	forename = names.name_query(race, gender, 1)
+	surname = names.name_query(race, 's', 1)
+	full_name = '-'.join(forename + surname)
+	
+	stats = dice.parse('!roll 4d6l1^6')
+	
+	print(full_name, stats)
+	
+	
---
+++ gentest.py
@@ -0,0 +1,3 @@
+import gen
+
+print(gen.parse('!gen'))
